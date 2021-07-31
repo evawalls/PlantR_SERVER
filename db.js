4f113c0d0dbd18ae2b3dc80cef77087ddb273301
Eva Walls <evawalls@evas-mbp.lan>
2021-07-31 04:11:19 -0400

    Models

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL,{
     
-    host:"localhost",
+    
     dialect: "postgres", 
     dialectOptions: {
         ssl: {
